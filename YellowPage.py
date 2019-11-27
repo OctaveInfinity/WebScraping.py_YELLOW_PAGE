57284cd6829a534d52bc26bba8deeb657be975e3
OctaveInfinity <e.makoviak@gmail.com>
2019-11-27 09:14:56 +0200

    Add Pages and Numberd_name

---
+++ YellowPage.py
@@ -0,0 +1,20 @@
+from requests_html import HTML, HTMLSession
+
+MAX_PAGE = 3
+
+session = HTMLSession()
+
+for page in range(1, MAX_PAGE+1):
+    page = f'https://www.yellowpages.com/austin-tx/plumbers?page={page}'
+    response = session.get(page)
+
+    companies = response.html.find('div.info')
+
+    for company in companies:
+
+        # numbered_name ------------------------------------------------	
+        numbered_name = company.find('.n', first=True).text
+        if numbered_name.find('. ') < 0:
+            continue
+
+        print(numbered_name)
